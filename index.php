464fc6b845884288401fafccb64355e2b8fe334b
Monang Bahana Harsuda <58503047+adekmo@users.noreply.github.com>
2019-12-15 21:38:52 +0700

    Add files via upload
    hanya kodingan tanpa image & file bootstrap.

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html leng="en">
+<head>
+	<!-- meta tag yang di perlukan -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+
+	 <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="./embed/fonts/fontawesome5-overrides.min.css" />
+    <link rel="stylesheet" href="./embed/fonts/fontawesome5-overrides.min.css" />
+    <link rel="stylesheet" href="./embed/fonts/fontawesome5-overrides.min.css" />
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+	<title>Website Simple</title>
+</head>
+<body style="background-color: rgb(145,143,143);color: rgb(192,195,197);">
+    <nav class="navbar navbar-light navbar-expand-md">
+        <div class="container-fluid">
+            <nav class="navbar navbar-light navbar-expand-md d-flex flex-fill navigation-clean-search" style="background-color: rgb(105,108,113);">
+                <div class="container-fluid"><a class="navbar-brand" href="#" style="font-family: Acme, sans-serif;color: rgba(242,239,239,0.9);">Monang Bahana Harsuda<br />         FrontEnd Dev</a><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div
+                        class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="nav navbar-nav">
+                            <li role="presentation" class="nav-item"><a class="nav-link active" href="#profile" style="color: rgb(255,255,255);">Profile</a></li>
+                            <li role="presentation" class="nav-item"><a class="nav-link" href="#gallery" style="color: rgb(255,255,255);">Gallery</a></li>
+                            <li role="presentation" class="nav-item"><a class="nav-link" href="#location" style="color: rgb(255,255,255);">Location</a></li>
+                        </ul>
+                        <form class="form-inline mr-auto" target="_self">
+                            <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input type="search" class="form-control search-field" id="search-field" name="search" /></div>
+                        </form><a class="btn btn-light action-button" role="button" href="#">Action </a></div>
+        </div>
+        </nav>
+        </div>
+    </nav>
+    <div class="highlight-phone" style="background-color: rgb(254,253,253);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="intro">
+                        <h2 style="color: rgb(40,41,42);">Hai, I&#39;m Monang</h2>
+                        <p style="color: rgb(50,49,49);font-family: Acme, sans-serif;">I&#39;m 27 years old. This is my prototipe<br />I like to design the website. Hopefully this work can help me to find the best Job. </p>
+                    </div>
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item"></li>
+                        <li class="nav-item"></li>
+                        <li class="nav-item"></li>
+                    </ul>
+                    <figure class="figure"></figure><a href="#"></a>
+                    <figure class="figure"></figure>
+                </div>
+                <div class="col-sm-4">
+                    <div class="d-none d-md-block iphone-mockup"><img class="rounded-circle device" src="img/1559328094505.jpg" width="300" height="250" /></div>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <section id="profile">
+    <div class="features-blue" style="background-color: rgb(145,143,143);">
+        <div class="container" style="background-color: #918f8f;">
+            <div class="intro">
+                <h2 class="text-center" style="background-color: #918f8f;">Profile</h2>
+            </div>
+            <div class="row features">
+                <div class="col-sm-6 col-md-4 item"><i class="fa fa-map-marker icon"></i>
+                    <h3 class="name">Address</h3>
+                    <p class="description" style="color: rgba(255,255,255,0.8);">Jalan Zeni AD VB No 12A. Kalibata. Jakarta Selatan</p>
+                </div>
+                <div class="col-sm-6 col-md-4 item"><i class="fa fa-clock-o icon"></i>
+                    <h3 class="name">Education</h3>
+                    <p class="description" style="color: rgba(255,255,255,0.8);">2008 - 2011    SMAN 1 Kecamatan Guguak<br />2011 - 2014    AMIK BSI Bogor<br />                       -Komputer Akuntansi<br />2016 - 2018    Universitas Widyatama<br />                       -Sistem Informasi</p>
+                </div>
+                <div class="col-sm-6 col-md-4 item"><i class="fa fa-list-alt icon"></i>
+                    <h3 class="name">Training</h3>
+                    <p class="description" style="color: rgba(255,255,255,0.8);">Pelatihan Web Developer di BBPLK Bekasi<br />(Maret - Mei 2019)<br /><br /></p>
+                </div>
+                <div class="col-sm-6 col-md-4 item"><i class="fa fa-leaf icon"></i>
+                    <h3 class="name">Contact</h3>
+                    <p class="description" style="color: rgba(255,255,255,0.8);">085835560537<br />082122389046(WA)<br />monangbahana@gmail.com</p>
+                </div>
+                <div class="col-sm-6 col-md-4 item"><i class="fa fa-plane icon"></i>
+                    <h3 class="name">Computer Skills</h3>
+                    <p class="description">Ms Office<br />Adobe Photoshop<br />Adobe Premiere<br />Adobe Lightroom<br />Adobe Dreamweaver<br />Bootsstrap Studio<br /></p>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-4 offset-lg-0 item"><i class="fa fa-phone icon"></i>
+                    <h3 class="name">Programming Skills</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">90%</div>
+                    </div>
+                    <p style="color: rgba(255,255,255,0.8);">HTML</p>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">80%</div>
+                    </div>
+                    <p style="color: rgba(255,255,255,0.8);">CSS</p>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">60%</div>
+                    </div>
+                    <p style="color: rgba(255,255,255,0.8);">PHP</p>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">40%</div>
+                    </div>
+                    <p style="color: rgba(255,255,255,0.8);">JavaScript</p>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">50%</div>
+                    </div>
+                    <p style="color: rgba(255,255,255,0.8);">Bootstrap</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+    <br>
+    <br>
+    <section id="gallery">
+    <div class="flex-fill photo-gallery">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Gallery</h2>
+                <p class="text-center">This my galery about my another work as  Photographer &amp; Photo editor.</p>
+            </div>
+            <div class="row photos">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href="desk.jpg"></a><img src="img/1501782784797.jpg" style="width: 300;height: 300;" width="250" height="200" /></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href="building.jpg"></a><img src="img/1491241333867.jpg" width="250" height="200" /></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href="loft.jpg"></a><img src="img/1485317765353.jpg" width="250" height="200" /></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href="building.jpg"></a><img src="img/1475566170536.jpg" width="250" height="200" /></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href="loft.jpg"></a><img src="img/IMG_20160624_010502.jpg" width="250" height="200" /></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href="desk.jpg"></a><img src="img/IMG_20161117_010312.jpg" width="250" height="200" /></div>
+            </div>
+        </div>
+    </div>
+    </section>
+    <br>
+    <br>
+    <section id="location">
+    <div class="contact-clean"></div>
+    <div class="map-clean">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Location </h2>
+                <p class="text-center"></p>
+            </div>
+        </div><iframe allowfullscreen frameborder="0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDmOZRQ5xOEmGisRH_U-t8DPZDsjOVrbvU&amp;q=Jakarta&amp;zoom=15" width="100%" height="450"></iframe></div>
+        </section>
+    <div class="footer-basic">
+        <footer class="flex-fill">
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Profile</a></li>
+                <li class="list-inline-item"><a href="#">Gallery</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">Monang Bahana Harsuda© 2019</p>
+        </footer>
+    </div>
+    <div></div>
+</body>
+</html>
